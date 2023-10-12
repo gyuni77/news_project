951bfab06449168f829a84f91053be8211a27efd
제동균 <gyuni@jedong-gyun-ui-MacBookAir.local>
2023-10-12 17:48:18 +0900

    feat: 헤더 일정 작업 완료

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./style/Header.css";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-  return <div className="Header">헤더</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="Header">
+      <img
+        src={process.env.PUBLIC_URL + "../../assets/news.png"}
+        alt="logo"
+        style={{ width: "15%", cursor: "pointer" }}
+        onClick={navigate("/")}
+      />
+      <input className="inputTitle" placeholder="제목을 입력하세요" />
+      <img
+        src={process.env.PUBLIC_URL + "../../assets/user.png"}
+        alt="user"
+        style={{ width: "15%", cursor: "pointer" }}
+      />
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { BsNewspaper } from "react-icons/bs";
 import "./style/Login.css";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <div className="Login">
+      <div>하루에 한번 뉴스를 요약해서 봐요</div>
       <BsNewspaper size="100" />
       <img
+        className="kakaoBtn"
         src={process.env.PUBLIC_URL + "../../assets/kakao.png"}
         alt="kakao"
       />
-      <div>비회원으로 로그인 하기</div>
+      <div onClick={navigate("/main")}>비회원으로 로그인 하기</div>
     </div>
   );
 }
